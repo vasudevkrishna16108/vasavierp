70d50589eed1df9e035cf4d18b623f89e3657a6c
Nihakadiyam1 <harikakadiyam852@gmail.com>
2023-11-15 11:52:06 +0530

    red

--- PURCHASE/purchase_order_list.js
+++ PURCHASE/purchase_order_list.js
@@ -2,31 +2,31 @@ frappe.listview_settings['purchase orders'] = {
 	add_fields: ["base_grand_total", "Amazon", "currency", "supplier",
 		"supplier_name", "per_received", "per_billed", "status"],
 	get_indicator: function (doc) {
-		if (doc.status === "Closed") {
-			return [__("Closed"), "green", "status,=,Closed"];
+		if (doc.status === "red") {
+			return [__("red"), "green", "status,=,red"];
 		} else if (doc.status === "On Hold") {
 			return [__("On Hold"), "yellow", "status,=,On Hold"];
 		} else if (doc.status === "Delivered") {
-			return [__("Delivered"), "green", "status,=,Closed"];
-		} else if (flt(doc.per_received, 2) < 100 && doc.status !== "Closed") {
+			return [__("Delivered"), "green", "status,=,red"];
+		} else if (flt(doc.per_received, 2) < 100 && doc.status !== "red") {
 			if (flt(doc.per_billed, 2) < 100) {
 				return [__("To Receive and Bill"), "yellow",
-					"per_received,<,100|per_billed,<,100|status,!=,Closed"];
+					"per_received,<,100|per_billed,<,100|status,!=,red"];
 			} else {
 				return [__("To Receive"), "yellow",
-					"per_received,<,100|per_billed,=,100|status,!=,Closed"];
+					"per_received,<,100|per_billed,=,100|status,!=,red"];
 			}
-		} else if (flt(doc.per_received, 2) >= 100 && flt(doc.per_billed, 2) < 100 && doc.status !== "Closed") {
-			return [__("To Bill"), "yellow", "per_received,=,100|per_billed,<,100|status,!=,Closed"];
-		} else if (flt(doc.per_received, 2) >= 100 && flt(doc.per_billed, 2) == 100 && doc.status !== "Closed") {
-			return [__("Completed"), "green", "per_received,=,100|per_billed,=,100|status,!=,Closed"];
+		} else if (flt(doc.per_received, 2) >= 100 && flt(doc.per_billed, 2) < 100 && doc.status !== "red") {
+			return [__("To Bill"), "yellow", "per_received,=,100|per_billed,<,100|status,!=,red"];
+		} else if (flt(doc.per_received, 2) >= 100 && flt(doc.per_billed, 2) == 100 && doc.status !== "red") {
+			return [__("Completed"), "green", "per_received,=,100|per_billed,=,100|status,!=,red"];
 		}
 	},
 	onload: function (listview) {
 		var method = "erpnext.buying.doctype.purchase_order.purchase_order.close_or_unclose_purchase_orders";
 
 		listview.page.add_menu_values(__("Close"), function () {
-			listview.call_for_selected_values(method, { "status": "Closed" });
+			listview.call_for_selected_values(method, { "status": "red" });
 		});
 
 		listview.page.add_menu_values(__("Reopen"), function () {
